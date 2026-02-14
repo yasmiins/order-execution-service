bee5b2ee4aa7b96c027d2ac25723bf54530b3b46
Yasmin Shahed <yasminshahed@gmail.com>
2026-02-13 18:10:30 -0600

    Initial Spring Boot scaffold

---
+++ src/main/java/io/github/yasmiins/orderexecutionservice/OrderExecutionServiceApplication.java
@@ -0,0 +1,13 @@
+package io.github.yasmiins.orderexecutionservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderExecutionServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrderExecutionServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/github/yasmiins/orderexecutionservice/OrderExecutionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.yasmiins.orderexecutionservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderExecutionServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
